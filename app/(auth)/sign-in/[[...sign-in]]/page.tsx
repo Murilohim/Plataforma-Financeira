b7951a898826e65dc8822b0c279be39707c8b2eb
murilohim <murilo.terenciani@gmail.com>
2024-06-17 02:25:31 -0300

    feat: estilizando tela de sign in

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,18 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-    return <SignIn />;
+    return (
+        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+            <div className="h-full lg:flex flex-col items-center justify-center px-4">
+                <div className="text-center space-y-4 pt-16">
+                    <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome back!</h1>
+                    <p className="text-base text-[#7E8CA0]">Sign in to your account or create a new account if you don&apos;t have one to get back to your dashboard.
+                    </p>
+                </div>
+                <div className="flex items-center justify-center mt-8">
+                    <SignIn />
+                </div>
+            </div>
+        </div>
+    );
 }
