4a92624bb9e846525033628a4d844e1b16ecdff5
murilohim <murilo.terenciani@gmail.com>
2024-07-13 19:19:39 -0300

    feat: hook de atualizar conta

---
+++ features/accounts/api/use-edit-account.ts
@@ -0,0 +1,36 @@
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.accounts[":id"]["$patch"]>;
+type RequestType = InferRequestType<typeof client.api.accounts[":id"]["$patch"]>["json"]
+
+export const useEditAccount = (id?: string) => {
+    const queryClient = useQueryClient()
+
+    const mutation = useMutation<ResponseType, Error, RequestType>({
+        mutationFn: async (json) => {
+            const response = await client.api.accounts[":id"]["$patch"]({
+                param: { id },
+                json,
+            })
+
+            return await response.json()
+        },
+        onSuccess: () => {
+            toast.success('Conta editada com sucesso.')
+            queryClient.invalidateQueries({
+                queryKey: ['account', { id }]
+            })
+            queryClient.invalidateQueries({
+                queryKey: ['accounts']
+            })
+        },
+        onError: () => {
+            toast.error('Erro ao editar conta.')
+        }
+    })
+
+    return mutation
+}
