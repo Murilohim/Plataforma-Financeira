dc5197f75ab5c580fdb59767d938d9ca818846aa
murilohim <murilo.terenciani@gmail.com>
2024-07-13 16:16:38 -0300

    feat: textos

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -7,8 +7,11 @@ export default function Page() {
         <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
             <div className="h-full lg:flex flex-col items-center justify-center px-4">
                 <div className="text-center space-y-4 pt-16">
-                    <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome back!</h1>
-                    <p className="text-base text-[#7E8CA0]">Sign in to your account or create a new account if you don&apos;t have one to get back to your dashboard.
+                    <h1 className="font-bold text-3xl text-[#2E2A47]">
+                        Bem-vindo de volta!
+                    </h1>
+                    <p className="text-base text-[#7E8CA0]">
+                        Faça login na sua conta ou crie uma nova conta se você não tiver uma para voltar ao seu dashboard.
                     </p>
                 </div>
                 <div className="flex items-center justify-center mt-8">
--- components/welcome-msg.tsx
+++ components/welcome-msg.tsx
@@ -8,10 +8,10 @@ export const WelcomeMsg = () => {
     return (
         <div className="space-y-2 mb-4">
             <h2 className="text-2xl lg:text-4xl text-white font-medium">
-                Welcome Back{isLoaded ? ", " : " "}{user?.firstName} 🤘
+                Bem vindo de volta{isLoaded ? ", " : " "}{user?.firstName} 🤘
             </h2>
             <p className="text-sm lg:text-base text-[#89B6FD]">
-                This is your Financial Overview Report
+                Este é o seu Relatório de Visão Geral Financeira
             </p>
         </div>
     )
