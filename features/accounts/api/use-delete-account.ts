79fb89cdbce9ad3b322fe051537b7cb54d761c39
murilohim <murilo.terenciani@gmail.com>
2024-07-13 19:24:45 -0300

    feat: hook de deleção de conta

---
+++ features/accounts/api/use-delete-account.ts
@@ -0,0 +1,34 @@
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.accounts[":id"]["$delete"]>;
+
+export const useDeleteAccount = (id?: string) => {
+    const queryClient = useQueryClient()
+
+    const mutation = useMutation<ResponseType, Error>({
+        mutationFn: async () => {
+            const response = await client.api.accounts[":id"]["$delete"]({
+                param: { id },
+            })
+
+            return await response.json()
+        },
+        onSuccess: () => {
+            toast.success('Conta deletada com sucesso.')
+            queryClient.invalidateQueries({
+                queryKey: ['account', { id }]
+            })
+            queryClient.invalidateQueries({
+                queryKey: ['accounts']
+            })
+        },
+        onError: () => {
+            toast.error('Erro ao deletar conta.')
+        }
+    })
+
+    return mutation
+}
