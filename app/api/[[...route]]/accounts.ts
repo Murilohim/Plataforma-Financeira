cfda176815cf2e2599336a83a9f1312ccad602ab
murilohim <murilo.terenciani@gmail.com>
2024-07-13 02:44:46 -0300

    feat: criação do endpoint post de accounts e hook de criar contas

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,10 @@
 import { db } from '@/db/drizzle'
-import { accounts } from '@/db/schema'
+import { accounts, insertAccountsSchema } from '@/db/schema'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { eq } from 'drizzle-orm'
 import { Hono } from 'hono'
+import { zValidator } from "@hono/zod-validator"
+import { createId } from "@paralleldrive/cuid2"
 
 const app = new Hono()
     .get(
@@ -25,5 +27,32 @@ const app = new Hono()
 
             return c.json({ data })
         })
+    .post(
+        "/",
+        clerkMiddleware(),
+        zValidator("json", insertAccountsSchema.pick({
+            name: true,
+        })),
+        async (c) => {
+            const auth = getAuth(c)
+            const values = c.req.valid("json")
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401)
+            }
+
+            const [data] = await db
+                .insert(accounts)
+                .values({
+                    id: createId(),
+                    userId: auth.userId,
+                    ...values,
+                })
+                .returning()
+
+
+            return c.json({ data })
+        }
+    )
 
 export default app
---
+++ features/accounts/api/use-create-account.ts
@@ -0,0 +1,30 @@
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.accounts.$post>;
+type RequestType = InferRequestType<typeof client.api.accounts.$post>["json"]
+
+export const useCreateAccount = () => {
+    const queryClient = useQueryClient()
+
+    const mutation = useMutation<ResponseType, Error, RequestType>({
+        mutationFn: async (json) => {
+            const response = await client.api.accounts.$post({ json })
+
+            return await response.json()
+        },
+        onSuccess: () => {
+            toast.success('Conta criada com sucesso')
+            queryClient.invalidateQueries({
+                queryKey: ['accounts']
+            })
+        },
+        onError: () => {
+            toast.error('Erro ao criar conta')
+        }
+    })
+
+    return mutation
+}
