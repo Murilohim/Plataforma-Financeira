e43bc507e29387a0e9bf2aae627f4832b25ffb90
murilohim <murilo.terenciani@gmail.com>
2024-07-15 03:47:32 -0300

    feat: card de importação e chamada na tela

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -9,8 +9,37 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+import { useState } from "react";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+
+
+enum VARIANTS {
+    LIST = "LIST",
+    IMPORT = "IMPORT"
+}
+
+const INITIAL_IMPORT_RESULTS = {
+    data: [],
+    errors: [],
+    meta: {}
+}
 
 const TransactionsPage = () => {
+
+    const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST)
+    const [importResults, setImportResults] = useState<typeof INITIAL_IMPORT_RESULTS>(INITIAL_IMPORT_RESULTS)
+
+    const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+        console.log({ results })
+        setImportResults(results)
+        setVariant(VARIANTS.IMPORT)
+    }
+
+    const onCancelImport = () => {
+        setImportResults(INITIAL_IMPORT_RESULTS)
+        setVariant(VARIANTS.LIST)
+    }
 
     const { onOpen } = useNewTransaction()
     const { data: transactionsData, isLoading: isLoadingTransactions } = useGetTransactions()
@@ -45,6 +74,18 @@ const TransactionsPage = () => {
         )
     }
 
+    if (variant === VARIANTS.IMPORT) {
+        return (
+            <>
+                <ImportCard
+                    data={importResults.data}
+                    onCancel={onCancelImport}
+                    onSubmit={() => { }}
+                />
+            </>
+        )
+    }
+
     return (
         <div
             className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24"
@@ -60,13 +101,21 @@ const TransactionsPage = () => {
                     >
                         Histórico de transações
                     </CardTitle>
-                    <Button
-                        size={"sm"}
-                        onClick={onOpen}
+                    <div
+                        className="flex flex-col lg:flex-row gap-y-2 items-center gap-x-2"
                     >
-                        <Plus className="size-4 mr-2" />
-                        Adicionar nova transação
-                    </Button>
+                        <Button
+                            size={"sm"}
+                            onClick={onOpen}
+                            className="w-full lg:w-auto"
+                        >
+                            <Plus className="size-4 mr-2" />
+                            Adicionar nova transação
+                        </Button>
+                        <UploadButton
+                            onUpload={onUpload}
+                        />
+                    </div>
                 </CardHeader>
                 <CardContent>
                     <DataTable
