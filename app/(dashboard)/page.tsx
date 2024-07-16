db857c7ad9fa112a66cd7a92dc1c179bc46df6a2
murilohim <murilo.terenciani@gmail.com>
2024-07-16 00:18:29 -0300

    feat: chamada no dashboard de cards

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,16 +1,13 @@
-"use client"
+import { DataGrid } from "@/components/data-grid";
 
-import { Button } from "@/components/ui/button";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
+export default function DashboardPage() {
 
-export default function Home() {
-  const { onOpen } = useNewAccount()
 
   return (
-    <div>
-      <Button onClick={onOpen}>
-        Adicionar uma nova conta
-      </Button>
+    <div
+      className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24"
+    >
+      <DataGrid />
     </div>
   );
 }
