53b8465a82bec3bcae17ac900ce9e8c901ce8b79
murilohim <murilo.terenciani@gmail.com>
2024-07-16 02:42:29 -0300

    feat: loading e chamada na tela

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,10 +1,11 @@
-import { AreaChart, BarChart, FileSearch, LineChart } from "lucide-react"
+import { AreaChart, BarChart, FileSearch, LineChart, Loader2 } from "lucide-react"
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
 import { AreaVariant } from "./area-variant"
 import { BarVariant } from "./bar-variant"
 import { LineVariant } from "./line-variant"
 import { useState } from "react"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
+import { Skeleton } from "./ui/skeleton"
 
 type Props = {
     data?: {
@@ -130,6 +131,35 @@ export const Chart = ({ data }: Props) => {
                         )}
                     </>
                 )}
+            </CardContent>
+        </Card>
+    )
+}
+
+export const ChartLoading = () => {
+
+    return (
+        <Card
+            className="border-none drop-shadow-sm"
+        >
+            <CardHeader
+                className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between"
+            >
+                <Skeleton
+                    className="w-48 h-8"
+                />
+                <Skeleton
+                    className="h-8 lg:w-[120px] w-full"
+                />
+            </CardHeader>
+            <CardContent>
+                <div
+                    className="h-[350px] w-full flex items-center justify-center"
+                >
+                    <Loader2
+                        className="h-6 w-6 text-slate-300 animate-spin"
+                    />
+                </div>
             </CardContent>
         </Card>
     )
--- components/data-charts.tsx
+++ components/data-charts.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { useGetSummary } from "@/features/summary/api/use-get-summary"
-import { Chart } from "./chart"
+import { Chart, ChartLoading } from "./chart"
+import { SpendingPie, SpendingPieLoading } from "./spending-pie"
 
 export const DataCharts = () => {
 
@@ -9,8 +10,19 @@ export const DataCharts = () => {
 
     if (isLoadingSummary) {
         return (
-            <div>
-                Loading...
+            <div
+                className="grid grid-cols-1 lg:grid-cols-6 gap-8"
+            >
+                <div
+                    className="col-span-1 lg:col-span-3 xl:col-span-4"
+                >
+                    <ChartLoading />
+                </div>
+                <div
+                    className="col-span-1 lg:col-span-3 xl:col-span-2"
+                >
+                    <SpendingPieLoading />
+                </div>
             </div>
         )
     }
@@ -24,6 +36,13 @@ export const DataCharts = () => {
             >
                 <Chart
                     data={summaryData?.days}
+                />
+            </div>
+            <div
+                className="col-span-1 lg:col-span-3 xl:col-span-2"
+            >
+                <SpendingPie
+                    data={summaryData?.categories}
                 />
             </div>
         </div>
