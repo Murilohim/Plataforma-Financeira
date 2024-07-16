3234370d2f9421b5f1a8d524ee2821234938df2e
murilohim <murilo.terenciani@gmail.com>
2024-07-16 01:41:19 -0300

    feat: criação de consumo de data, componente chart e implementação na tela

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,4 @@
+import { DataCharts } from "@/components/data-charts";
 import { DataGrid } from "@/components/data-grid";
 
 export default function DashboardPage() {
@@ -8,6 +9,7 @@ export default function DashboardPage() {
       className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24"
     >
       <DataGrid />
+      <DataCharts />
     </div>
   );
 }
---
+++ components/data-charts.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { useGetSummary } from "@/features/summary/api/use-get-summary"
+import { Chart } from "./chart"
+
+export const DataCharts = () => {
+
+    const { data: summaryData, isLoading: isLoadingSummary } = useGetSummary()
+
+    if (isLoadingSummary) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
+    return (
+        <div
+            className="grid grid-cols-1 lg:grid-cols-6 gap-8"
+        >
+            <div
+                className="col-span-1 lg:col-span-3 xl:col-span-4"
+            >
+                <Chart
+                    data={summaryData?.days}
+                />
+            </div>
+        </div>
+    )
+}
